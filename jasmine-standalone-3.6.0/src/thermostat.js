f24c0c1bc51ae3298f1267db7e5eeb0e79dfdbd0
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-24 13:30:41 +0000

    few minor changes

--- jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine-standalone-3.6.0/src/thermostat.js
@@ -75,33 +75,32 @@ $(document).ready(function() {
   $('#temperature-up').click(function() {
     thermostat.up();
     updateTemperature();
-  })
+  });
 
   $('#temperature-down').click(function() {
     thermostat.down();
     updateTemperature();
-  })
+  });
 
   $('#temperature-reset').click(function() {
     thermostat.resetTemperature();
     updateTemperature();
-  })
+  });
 
   $('#powersaving-on').click(function() {
     thermostat.switchPowerSavingModeOn();
     $('#power-saving-status').text('on')
     updateTemperature();
-  })
+  });
 
   $('#powersaving-off').click(function() {
     thermostat.switchPowerSavingModeOff();
     $('#power-saving-status').text('off')
     updateTemperature();
-  })
+  });
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature());
     $('#temperature').attr('class', thermostat.energyUsage());
   }
-})
-
+}); 
