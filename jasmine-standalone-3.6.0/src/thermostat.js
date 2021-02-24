86710690b18ce94de1240f89372c3fd66d89f5f4
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-24 11:12:08 +0000

    8 tests passing

--- jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine-standalone-3.6.0/src/thermostat.js
@@ -2,6 +2,52 @@
 
 class Thermostat {
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
+    this.PSM_MAX_ON = 25;
+    this.PSM_MAX_OFF = 32;
     this.temperature = 20;
+    this.powerSavingMode = true;
   }
-}
+
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+
+  up() {  
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1;
+  }
+
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+    this.temperature -= 1;
+  }
+
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.PSM_MAX_OFF;
+    }
+    return this.temperature === this.PSM_MAX_ON;
+  }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
+};
